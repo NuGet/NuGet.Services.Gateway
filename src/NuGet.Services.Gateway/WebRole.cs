d7fc226a2c2f92401ecefa06865a69a29fbda9e2
anurse <andrew@andrewnurse.net>
2014-05-14 15:02:42 -0700

    Added some code *gasp* to rewrite web.config on start

--- src/NuGet.Services.Gateway/WebRole.cs
+++ src/NuGet.Services.Gateway/WebRole.cs
@@ -24,6 +24,14 @@ namespace NuGet.Services.Gateway
                     proxySection.SetAttributeValue("enabled", true);
                     server.CommitChanges();
                     Trace.TraceInformation("Enabled Reverse Proxy");
+
+                    // Patch web.config rewrite rules
+                    string serviceStem = RoleEnvironment.GetConfigurationSettingValue("Gateway.ServiceStem");
+                    if (!String.Equals(serviceStem, "nuget.org", StringComparison.OrdinalIgnoreCase))
+                    {
+                        RewriteStem(server, serviceStem);
+                    }
+                    server.CommitChanges();
                 }
 
                 return base.OnStart();
@@ -32,6 +40,37 @@ namespace NuGet.Services.Gateway
             {
                 Trace.TraceError("Error starting gateway: {0}", ex.ToString());
                 throw;
+            }
+        }
+
+        private void RewriteStem(ServerManager server, string serviceStem)
+        {
+            // Get the site config
+            const string siteBaseName = "Web"; // From the CSDEF file
+            string siteName =
+                RoleEnvironment.CurrentRoleInstance.Id + "_" + siteBaseName;
+            var config = server.Sites[siteName].GetWebConfiguration();
+
+            var rewriteSection = config.GetSection("system.webServer/rewrite");
+            if (rewriteSection == null)
+            {
+                var rewriteRules = rewriteSection.GetChildElement("rules");
+                if (rewriteRules != null)
+                {
+                    foreach (var rewriteRule in rewriteRules.ChildElements)
+                    {
+                        var action = rewriteRules.GetChildElement("action");
+                        if (action != null)
+                        {
+                            string url = action.GetAttributeValue("url");
+                            string rewritten = url.Replace("nuget.org", serviceStem);
+                            if (!String.Equals(url, rewritten, StringComparison.Ordinal))
+                            {
+                                action.SetAttributeValue("url", rewritten);
+                            }
+                        }
+                    }
+                }
             }
         }
     }
